b15642a66e5ca99a36f7834e8e57c0d41fa92a10
Ruud Verbij <github@ruudverbij.nl>
2013-05-22 15:48:56 +0200

    diffv1.bat now deletes old folder

--- diffv1.bat
+++ diffv1.bat
@@ -2,5 +2,5 @@ set PATH=%PATH%;"C:\Program Files\Git\cmd"
 mkdir old
 set /P branch=Enter branch: %=%
 git show %branch%:paper.tex > old/paper.tex 
-latexdiff "old/paper.tex" "paper.tex" > old/diff_paper.tex 
-pause
+latexdiff "old/paper.tex" "paper.tex" > diff_paper.tex 
+rmdir old /s
