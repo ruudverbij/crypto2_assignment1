0439d2a0cf005c190aae40454df7accbb7961706
Ruud Verbij <github@ruudverbij.nl>
2013-05-22 14:45:47 +0200

    Paper ready to work with

--- test.bat
+++
@@ -1,6 +0,0 @@
-set PATH=%PATH%;"C:\Program Files\Git\cmd"
-mkdir old
-set /P branch=Enter branch: %=%
-git show %branch%:paper.tex > old/paper.tex 
-latexdiff "old/paper.tex" "paper.tex" > old/diff_paper.tex 
-pause
